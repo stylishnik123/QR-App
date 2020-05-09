0ba35c8325b16db4e4cb81d3236073d1870f692c
Nikhil Wadhwa <nikhilwadhwa887@gmail.com>
2020-05-09 13:57:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/qrapp1/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.qrapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
+public class MainActivity extends AppCompatActivity {
+    EditText qrvalue;
+    Button generateBtn,scanBtn;
+    ImageView qrImage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        qrvalue = findViewById(R.id.qrInput);
+        generateBtn = findViewById(R.id.generateBtn);
+        scanBtn = findViewById(R.id.scanBtn);
+        qrImage = findViewById(R.id.qrPlaceHolder);
+        generateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = qrvalue.getText().toString();
+                if(data.isEmpty()){
+                    qrvalue.setError("Value Required.");
+                }else {
+                    QRGEncoder qrgEncoder = new QRGEncoder(data,null, QRGContents.Type.TEXT,500);
+                    try {
+                        Bitmap qrBits = qrgEncoder.encodeAsBitmap();
+                        qrImage.setImageBitmap(qrBits);
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        });
+
+
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Scanner.class));
+            }
+        });
+
+    }
+}
